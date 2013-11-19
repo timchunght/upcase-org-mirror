4029f12d724a93645262798a8ab43fa84217c525
Ben Orenstein <ben.orenstein@gmail.com>
2013-11-19 14:52:02 -0500

    More deets

--- app/controllers/customer_explorer_controller.rb
+++ app/controllers/customer_explorer_controller.rb
@@ -3,6 +3,7 @@ class CustomerExplorerController < ApplicationController
   layout 'empty-body'
 
   def index
-    @churned_customers = Subscription.where('deactivated_on is not NULL').order('deactivated_on desc').limit(200).includes(:user)
+    @churned_customers = Subscription.where('deactivated_on is not NULL').where('user_id is not NULL').
+      order('deactivated_on desc').limit(100).includes(:user)
   end
 end
